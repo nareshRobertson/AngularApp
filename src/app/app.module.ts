0df9222f8fb5524f4002a6a5868070a462aad205
Jagdeep_dev <jagdeeps.developer@gmail.com>
2021-02-02 12:57:37 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListMenuItemsComponent } from './menuItems/list-menu-items/list-menu-items.component';
+import { CrateItemComponent } from './menuItems/crate-item.component';
+import { CartComponent } from './cart/cart.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CheckoutOrderComponent } from './checkout-order/checkout-order.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListMenuItemsComponent,
+        CrateItemComponent,
+        CartComponent,
+        WelcomeComponent,
+        CheckoutOrderComponent
+    ],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
